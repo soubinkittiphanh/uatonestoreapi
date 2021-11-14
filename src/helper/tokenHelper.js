1ec50e95f708ac9bd90fc43359aea1c191a37a66
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-14 12:27:39 +0700

    update user info

--- src/helper/tokenHelper.js
+++ src/helper/tokenHelper.js
@@ -1,10 +1,10 @@
 const Token=require('../config')
 const jwt=require('jsonwebtoken');
-const login=(u_name,u_id)=>{
+const login=(u_name,u_id,u_phone,u_email)=>{
     console.log('=>'+process.env.ACCESS_TOKEN_SECRET);
     const username={user:u_name};
     const accessToken=jwt.sign(username,Token.actksecret,{expiresIn:'30s'});
-    return {accessToken:accessToken,userName:u_name,userId:u_id}
+    return {accessToken:accessToken,userName:u_name,userId:u_id,u_phone:u_phone,u_email:u_email}
 }
 
 module.exports={login}
