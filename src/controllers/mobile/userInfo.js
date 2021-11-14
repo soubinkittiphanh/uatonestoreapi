8553c5471067c525d85e78a1a5f7b1846dcd1ccc
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-14 11:22:00 +0700

    update user info

--- src/controllers/mobile/userInfo.js
+++ src/controllers/mobile/userInfo.js
@@ -1,48 +1,51 @@
 const Db = require('../../config/dbcon');
 
-const updateUserName=async(req,res)=>{
+const updateUserName = async (req, res) => {
     console.log("update user name=====>");
     console.log(req.body);
-    const body=req.body
-    const userId=body.user_id;
-    const userName=body.user_name;
-   await Db.query(`UPDATE CUSTOMER SET cus_name='${userName}' WHERE cus_id='${userId}'`,(er,re)=>{
-        if(er)return res.send("Error: "+er.message);
+    const body = req.body
+    const userId = body.user_id;
+    const userName = body.user_name;
+    await Db.query(`UPDATE CUSTOMER SET cus_name='${userName}' WHERE cus_id='${userId}'`, (er, re) => {
+        if (er) {
+            console.log("Error:  ");
+            return res.send("Error: " + er.message);
+        }
         console.log("Transaction completed");
         res.send("Transaction completed");
     })
 }
-const updateTel=async(req,res)=>{
+const updateTel = async (req, res) => {
     console.log(req.body);
-    const body=req.body
-    const userId=body.user_id;
-    const userPhoneNumber=body.user_phone;
-    await Db.query(`UPDATE CUSTOMER SET cus_tel='${userPhoneNumber}' WHERE cus_id='${userId}'`,(er,re)=>{
-        if(er)return res.send("Error: "+er.message);
+    const body = req.body
+    const userId = body.user_id;
+    const userPhoneNumber = body.user_phone;
+    await Db.query(`UPDATE CUSTOMER SET cus_tel='${userPhoneNumber}' WHERE cus_id='${userId}'`, (er, re) => {
+        if (er) return res.send("Error: " + er.message);
         res.send("Transaction completed");
     })
 }
-const updateEmail=async(req,res)=>{
+const updateEmail = async (req, res) => {
     console.log(req.body);
-    const body=req.body
-    const userId=body.user_id;
-    const userEmail=body.user_email;
-    await Db.query(`UPDATE CUSTOMER SET cus_email='${userEmail}' WHERE cus_id='${userId}'`,(er,re)=>{
-        if(er)return res.send("Error: "+er.message);
+    const body = req.body
+    const userId = body.user_id;
+    const userEmail = body.user_email;
+    await Db.query(`UPDATE CUSTOMER SET cus_email='${userEmail}' WHERE cus_id='${userId}'`, (er, re) => {
+        if (er) return res.send("Error: " + er.message);
         res.send("Transaction completed");
     })
 }
-const updatePassword=async(req,res)=>{
+const updatePassword = async (req, res) => {
     console.log(req.body);
-    const body=req.body
-    const userId=body.user_id;
-    const userPass=body.user_password;
-    await Db.query(`UPDATE CUSTOMER SET cus_pass='${userPass}' WHERE cus_id='${userId}'`,(er,re)=>{
-        if(er)return res.send("Error: "+er.message);
+    const body = req.body
+    const userId = body.user_id;
+    const userPass = body.user_password;
+    await Db.query(`UPDATE CUSTOMER SET cus_pass='${userPass}' WHERE cus_id='${userId}'`, (er, re) => {
+        if (er) return res.send("Error: " + er.message);
         res.send("Transaction completed");
     })
 }
-module.exports={
+module.exports = {
     updateUserName,
     updateTel,
     updateEmail,
