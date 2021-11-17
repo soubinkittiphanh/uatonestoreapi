99e7b668e36af7c7be991037fc33c08ca2a44ca3
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-17 22:07:52 +0700

    product count stock

--- src/controllers/client/userOrder.js
+++ src/controllers/client/userOrder.js
@@ -1,87 +1,110 @@
 const Db = require('../../config/dbcon')
-const OrderHelper=require('../../helper/mobile/orderHelper')
-const createOrder=async(req,res)=>{
-    const body =req.body;
-    const user_id=body.user_id;
-    const cart_data=body.cart_data;
-    console.log("data: "+req.body.cart_data);
-    console.log("data usr_id: "+req.body.user_id);
-    
-    let i=0;
-    let sqlCom=`INSERT INTO user_order(order_id, user_id, product_id, product_amount, product_price, order_price_total) VALUES `;
+const OrderHelper = require('../../helper/mobile/orderHelper')
+const createOrder = async (req, res) => {
+    const body = req.body;
+    const user_id = body.user_id;
+    const cart_data = body.cart_data;
+    console.log("data: " + req.body.cart_data);
+    console.log("data usr_id: " + req.body.user_id);
+
+    let i = 0;
+    let sqlCom = `INSERT INTO user_order(order_id, user_id, product_id, product_amount, product_price, order_price_total) VALUES `;
     //Get last order_id
-    await Db.query('SELECT IFNULL(MAX(order_id),0) AS order_id FROM user_order;',(er,re)=>{
-        if(er) return res.send("Error: "+er)
+    await Db.query('SELECT IFNULL(MAX(order_id),0) AS order_id FROM user_order;', (er, re) => {
+        if (er) return res.send("Error: " + er)
         console.log("pass error:");
-        console.log("pass error:"+re[0]);
-        let genOrderId=re[0]['order_id'];
-        console.log("order_id: "+genOrderId);
-        if(genOrderId==0) genOrderId=10000;
-        else genOrderId=parseInt(genOrderId)+1;
-        console.log("len: "+cart_data.length);
-        cart_data.forEach(el=>{
+        console.log("pass error:" + re[0]);
+        let genOrderId = re[0]['order_id'];
+        console.log("order_id: " + genOrderId);
+        if (genOrderId == 0) genOrderId = 10000;
+        else genOrderId = parseInt(genOrderId) + 1;
+        console.log("len: " + cart_data.length);
+        cart_data.forEach(el => {
             // Check the weather the product is available in stok or not
-            let count_stock= OrderHelper.checkStockAvailability(el.product_id,el.product_amount);
-            if(count_stock!=200){
-                console.log("STOCK STATUS CODE: "+count_stock);
-                return res.send(count_stock==503?"ເກີດຂໍ້ຜິດພາດ ສິນຄ້າບໍ່ພຽງພໍ":"Connection Error");
+            let count_stock = checkStockAvailability(el.product_id, el.product_amount);
+            if (count_stock != 200) {
+                console.log("STOCK STATUS CODE: " + count_stock);
+                return res.send(count_stock == 503 ? "ເກີດຂໍ້ຜິດພາດ ສິນຄ້າບໍ່ພຽງພໍ" : "Connection Error");
             }
-            console.log("count_stock first: "+count_stock);
-            console.log("start i "+i);
-            if(i==cart_data.length-1){
+            console.log("count_stock first: " + count_stock);
+            console.log("start i " + i);
+            if (i == cart_data.length - 1) {
                 //Last row
-                sqlCom=sqlCom+`(${genOrderId},${user_id},${el.product_id},${el.product_amount},${el.product_price},${el.order_price_total});`;
-            }else{
-                sqlCom=sqlCom+`(${genOrderId},${user_id},${el.product_id},${el.product_amount},${el.product_price},${el.order_price_total}),`;
+                sqlCom = sqlCom + `(${genOrderId},${user_id},${el.product_id},${el.product_amount},${el.product_price},${el.order_price_total});`;
+            } else {
+                sqlCom = sqlCom + `(${genOrderId},${user_id},${el.product_id},${el.product_amount},${el.product_price},${el.order_price_total}),`;
             }
-            i=i+1;
-            
+            i = i + 1;
+
         });
-        let sqlComCardSale="";
-        console.log("SQL: "+sqlCom);
-        Db.query(sqlCom,(er,re)=>{
-            if(er)return res.send("Error: "+er);
+        let sqlComCardSale = "";
+        console.log("SQL: " + sqlCom);
+        Db.query(sqlCom, (er, re) => {
+            if (er) return res.send("Error: " + er);
             // If no error insert to order then we should insert to card_sale for mapping card_sale -> user_order -> card
             // let j=0;
             console.log("Ready to insert to card_sale");
-            cart_data.forEach(el=>{
+            cart_data.forEach(el => {
                 // console.log("start i "+j);
                 // if(j==cart_data.length-1){
-                sqlComCardSale=sqlComCardSale+`INSERT INTO card_sale(card_code,card_order_id) 
+                sqlComCardSale = sqlComCardSale + `INSERT INTO card_sale(card_code,card_order_id) 
                 SELECT c.card_number,'${genOrderId}' FROM card c WHERE c.card_isused =0 AND c.product_id='${el.product_id}' LIMIT ${el.product_amount};`
                 // }else{
 
                 // }
             })
-            Db.query(sqlComCardSale,(er,re)=>{
+            Db.query(sqlComCardSale, (er, re) => {
                 console.log("********Insert in to card_sale**********");
-                if(er)return res.send("Error: "+er)
+                if (er) return res.send("Error: " + er)
 
                 res.send("Transaction completed");
             })
         })
-        
+
     });
 }
-const updateOrder=async(req,res)=>{
-    
+const updateOrder = async (req, res) => {
+
 }
-const fetchOrder=async(req,res)=>{
-    const memId=req.query.mem_id;
-    
-    console.log("mem_id: "+memId);
-    await Db.query(`SELECT o.*,p.pro_name FROM user_order o LEFT JOIN product p on o.product_id=p.pro_id WHERE o.user_id ='${memId}' ORDER BY o.order_id DESC`,(er,re)=>{
-        if(er) return res.send("Error: "+er)
+const fetchOrder = async (req, res) => {
+    const memId = req.query.mem_id;
+
+    console.log("mem_id: " + memId);
+    await Db.query(`SELECT o.*,p.pro_name FROM user_order o LEFT JOIN product p on o.product_id=p.pro_id WHERE o.user_id ='${memId}' ORDER BY o.order_id DESC`, (er, re) => {
+        if (er) return res.send("Error: " + er)
         res.send(re);
     })
 
 
 }
-const fetchOrderById=async(req,res)=>{
+const fetchOrderById = async (req, res) => {
 
 }
+const checkStockAvailability = async (product_id, order_qty) => {
 
-module.exports={
+    // 200 = no error available
+    // 500 = SQL ERROR
+    // 503 = Product stock not suffient
+
+
+    const sqlCom = `SELECT d.product_id AS card_pro_id,COUNT(d.card_number)-COUNT(cs.card_code) AS card_count FROM card d
+    LEFT JOIN card_sale cs ON cs.card_code=d.card_number WHERE d.product_id ='${product_id}'
+    GROUP BY d.product_id`;
+    let stockCount = 0;
+    await Db.query(sqlCom, (er, re) => {
+        if (er) {
+            console.log("Stock check Error: " + er);
+            return 500
+        }
+        stockCount = re[0]["card_count"];
+        if (stockCount - order_qty < 0) {
+            return 503
+        }
+        return 200
+
+    })
+}
+module.exports = {
     createOrder,
     fetchOrder,
 }
