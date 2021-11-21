bd1963e1648b11456d5186ebabe984417fdaf5d5
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-21 18:30:29 +0700

    product count stockssss

--- src/controllers/client/userInbox.js
+++ src/controllers/client/userInbox.js
@@ -1,7 +1,7 @@
 const Db = require('../../config/dbcon')
 
 const fetchInbox=async(req,res)=>{
-    const body =req.body;
+    const body =req.query;
     const custId=body.cust_id;
     const sqlCom=`SELECT s.* FROM card_sale s WHERE s.card_order_id IN (SELECT order_id FROM user_order WHERE user_id='${custId}')`
     await Db.query(sqlCom,(er,re)=>{
