e2eed756a33ab8019d5ed4bd755dfbaef4a008a7
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-23 22:03:37 +0700

    REGISTER CUSTOMER

---
+++ src/controllers/client/register.js
@@ -0,0 +1,23 @@
+const Db = require('../../config/dbcon')
+
+const createCustomer=async(req,res)=>{
+    const body=req.body;
+    const cus_name=body.cust_name;
+    const cus_pass=body.cust_pass;
+    const cus_phone=body.cust_phone;
+    const cus_email=body.cust_email;
+    const cus_gameId=body.cust_gameId;
+
+    console.log("...Register customer...");
+    console.log("Info data: "+body);
+    const sqlCom =`INSERT INTO customer(cus_id, cus_pass, cus_name, cus_tel, cus_email, cus_active) VALUES ((SELECT IFNULL(MAX(c.cus_id),1000)+1 FROM customer c),'${cus_pass}','${cus_name}','${cus_phone}','${cus_email},1)`
+    await Db.query(sqlCom,(er,re)=>{
+        if(er)return res.send("Error: "+er);
+        res.send("Transaction completed");
+    })
+
+}
+
+module.exports={
+    createCustomer,
+}
