eddb9a63d15b1a6706248cc3526db934f71ae58e
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-11-27 08:28:54 +0700

    updage login id

--- src/controllers/client/register.js
+++ src/controllers/client/register.js
@@ -7,10 +7,11 @@ const createCustomer=async(req,res)=>{
     const cus_phone=body.cust_phone;
     const cus_email=body.cust_email;
     const cus_gameId=body.cust_gameId;
+    const login_id=cus_email?cus_phone:cus_email;
 
     console.log("...Register customer...");
     console.log("Info data: "+body);
-    const sqlCom =`INSERT INTO customer(cus_id, cus_pass, cus_name, cus_tel, cus_email, cus_active) VALUES ((SELECT IFNULL(MAX(c.cus_id),1000)+1 FROM customer c),'${cus_pass}','${cus_name}','${cus_phone}','${cus_email}',1)`
+    const sqlCom =`INSERT INTO customer(cus_id, cus_pass, cus_name, cus_tel, cus_email, cus_active,login_id) VALUES ((SELECT IFNULL(MAX(c.cus_id),1000)+1 FROM customer c),'${cus_pass}','${cus_name}','${cus_phone}','${cus_email}',1,'${login_id}')`
     await Db.query(sqlCom,(er,re)=>{
         if(er)return res.send("Error: "+er);
         res.send("Transaction completed");
