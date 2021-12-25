bded185f4e4ef6e1bfd235463d3f5acc4c1e3449
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2021-12-25 16:47:34 +0700

    update card_sale field

--- src/controllers/client/userInbox.js
+++ src/controllers/client/userInbox.js
@@ -2,6 +2,8 @@ const Db = require('../../config/dbcon')
 
 const fetchInbox=async(req,res)=>{
     const body =req.query;
+    console.log("************* LOAD INBOX *****************");
+    console.log(`*************Payload: ${body.cust_id} *****************`);
     const custId=body.cust_id;
     const sqlCom=`SELECT s.*,p.pro_category FROM card_sale s LEFT JOIN product p ON p.pro_id=s.pro_id WHERE s.card_order_id IN (SELECT order_id FROM user_order WHERE user_id='${custId}') ORDER BY s.id DESC`
     await Db.query(sqlCom,(er,re)=>{
@@ -11,6 +13,9 @@ const fetchInbox=async(req,res)=>{
 }
 const markReaded=async(req,res)=>{
     const body =req.body;
+    console.log("************* MARK INBOX AS READED *****************");
+    console.log(`*************Payload: ${body.card_number} *****************`);
+    
     const card_number=body.card_number;
     console.log("===>> "+card_number);
     await Db.query(`UPDATE card_sale SET mark_readed=1 WHERE card_code='${card_number}'`,(er,re)=>{
