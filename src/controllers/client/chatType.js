4db693d6c0453ea5e77e1c299161b6a0c34550b7
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-01-11 14:27:48 +0700

    update new chatype route

--- src/controllers/client/chatType.js
+++ src/controllers/client/chatType.js
@@ -2,13 +2,14 @@ const Db = require('../../config/dbcon')
 
 const createChatType=async(req,res)=>{
     const body =req.body;
-    const chat_type_id=body.chat_type_id;
+    
+    const chat_type_code=body.chat_type_code;
     const chat_type_name=body.chat_type_name;
     const chat_type_remart=body.chat_type_remart;
     console.log("************* CREATE CHAT TYPE *****************");
     console.log(`*************Payload: ${body.cust_id} *****************`);
     const custId=body.cust_id;
-    const sqlCom=`INSERT INTO chat_type( code, name, remark) VALUES ('${chat_type_id}','${chat_type_name}','${chat_type_remart}')`
+    const sqlCom=`INSERT INTO chat_type( code, name, remark) VALUES ('${chat_type_code}','${chat_type_name}','${chat_type_remart}')`
     await Db.query(sqlCom,(er,re)=>{
         if(er) return res.send("Error: "+er)
         res.send(re);
@@ -17,12 +18,13 @@ const createChatType=async(req,res)=>{
 const updateChatType=async(req,res)=>{
     const body =req.body;
     const chat_type_id=body.chat_type_id;
+    const chat_type_code=body.chat_type_code;
     const chat_type_name=body.chat_type_name;
     const chat_type_remart=body.chat_type_remart;
     console.log("************* UPDATE CHAT TYPE *****************");
     console.log(`*************Payload: ${body.chat_type_id} *****************`);
     
-    const sqlCom=`UPDATE chat_type SET  name='${chat_type_name}', remark='${chat_type_remart}' WHERE code='${chat_type_id}'`
+    const sqlCom=`UPDATE chat_type SET  name='${chat_type_name}', remark='${chat_type_remart}',code='${chat_type_code}' WHERE id=code='${chat_type_id}'`
     
     await Db.query(sqlCom,(er,re)=>{
         if(er) return res.send("Error: "+er);
