d70e44744cd5b84c827e04793816fa63ceb5db67
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-01-11 16:03:48 +0700

    update new chat route

--- src/controllers/client/chats.js
+++ src/controllers/client/chats.js
@@ -21,7 +21,10 @@ const markChatAsReaded = async (req, res) => {
 
     console.log("************* MARK CHAT  *****************");
     console.log(`*************Payload: ${body.chat_id} *****************`);
+    const currentDate=Date.now;
+    console.log("Date: "+currentDate.toString);
     const sqlCom = `UPDATE  chat SET chat_isread=1,read_time='' WHERE id=${chat_id}`
+    
     await Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
         res.send("Transaction completed");
