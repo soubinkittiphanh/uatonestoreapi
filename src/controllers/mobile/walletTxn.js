f45c01c9b99c2d50304b7e9ea808f5ab09acc058
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-01-30 13:28:09 +0700

    update order txn by date desc txn wallet

--- src/controllers/mobile/walletTxn.js
+++ src/controllers/mobile/walletTxn.js
@@ -4,7 +4,7 @@ const fetchWaletTxnCRnDR = async (req, res) => {
     console.log("************* FETCH WALLET TXN *****************");
     console.log(`*************Payload: ${req.query.user_id} *****************`);
     const userId = req.query.user_id;
-    const sqlCom=`SELECT h.txn_his_id,c.txn_sign,c.txn_code_name,h.txn_his_amount,h.txn_his_date FROM transaction_history h LEFT JOIN transaction t ON t.txn_id=h.txn_id LEFT JOIN transaction_code c ON c.txn_code_id=t.txn_code WHERE h.user_id='${userId}' ORDER BY h.txn_his_date`
+    const sqlCom=`SELECT h.txn_his_id,c.txn_sign,c.txn_code_name,h.txn_his_amount,h.txn_his_date FROM transaction_history h LEFT JOIN transaction t ON t.txn_id=h.txn_id LEFT JOIN transaction_code c ON c.txn_code_id=t.txn_code WHERE h.user_id='${userId}' ORDER BY h.txn_his_date DESC`
     await Db.query(sqlCom, (er, re) => {
         if (er) {
             console.log("Error:  ");
