47478c2d076f0c8cfd73400e4a8d7a83da5733f2
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-01-31 20:43:37 +0700

    update fetOrderByDate between

--- src/controllers/client/userOrder.js
+++ src/controllers/client/userOrder.js
@@ -105,11 +105,16 @@ const fetchOrder = async (req, res) => {
 }
 const fetchOrderByDate = async (req, res) => {
     const body = req.body;
-    const selectedDate = req.query.date
+    const fromDate = req.query.fromDate
+    const toDate = req.query.toDate
+    const userId = req.query.userId
     console.log("************* LOAD ORDER BY DATE *****************");
-    console.log(`*************Payload: ${selectedDate} *****************`);
-
-    await Db.query(`SELECT o.*,p.pro_name FROM user_order o LEFT JOIN product p on o.product_id=p.pro_id WHERE o.txn_date BETWEEN '${selectedDate}' AND '${selectedDate} 23:59:59' ORDER BY o.order_id DESC`, (er, re) => {
+    console.log(`*************Payload: ${fromDate} *****************`);
+    console.log(`*************Payload: ${toDate} *****************`);
+    console.log(`*************Payload: ${userId} *****************`);
+    const extraCondition=userId? ` AND o.user_id=${userId}`:''
+    const sqlCom=`SELECT o.*,p.pro_name FROM user_order o LEFT JOIN product p on o.product_id=p.pro_id WHERE o.txn_date BETWEEN '${fromDate}' AND '${toDate} 23:59:59' ${extraCondition}  ORDER BY o.order_id DESC`
+    await Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
         res.send(re);
     })
