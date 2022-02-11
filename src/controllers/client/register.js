d5b393debc89e2c9d06878446bc7610ad829b1d2
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-02-11 18:26:22 +0700

    update new register

--- src/controllers/client/register.js
+++ src/controllers/client/register.js
@@ -7,11 +7,15 @@ const createCustomer=async(req,res)=>{
     const cus_phone=body.cust_phone;
     const cus_email=body.cust_email;
     const cus_gameId=body.cust_gameId;
+    const village=body.cust_village;
+    const district=body.cust_district;
+    const province=body.cust_province;
+    const remark=body.cust_remark;
     const login_id=cus_email===''?cus_phone:cus_email;
 
     console.log("...Register customer...");
     console.log("Info data: "+body);
-    const sqlCom =`INSERT INTO customer(cus_id, cus_pass, cus_name, cus_tel, cus_email, cus_active,login_id) VALUES ((SELECT IFNULL(MAX(c.cus_id),1000)+1 FROM customer c),'${cus_pass}','${cus_name}','${cus_phone}','${cus_email}',1,'${login_id}')`
+    const sqlCom =`INSERT INTO customer(cus_id, cus_pass, cus_name, cus_tel, cus_email, cus_active,login_id,village,district,province,remark) VALUES ((SELECT IFNULL(MAX(c.cus_id),1000)+1 FROM customer c),'${cus_pass}','${cus_name}','${cus_phone}','${cus_email}',1,'${login_id}','${village}','${district}','${province}','${remark}')`
     await Db.query(`SELECT cus_id FROM customer WHERE login_id='${login_id}'`,(er,re)=>{
         if(er)return res.send("Error: "+ er);
         console.log("LEN: "+re.length);
