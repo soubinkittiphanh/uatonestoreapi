31c5a66c84f73606b2cf69dd8f5ce0d792395abc
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-11 23:30:45 +0700

    new report transaction dssS

--- src/controllers/admin/report/index.js
+++ src/controllers/admin/report/index.js
@@ -1,11 +1,10 @@
 const Db=require("../../../config/dbcon");
 
 const txnReport=async(req,res)=>{
-
+    console.log("**************** REPORT **************");
+    console.log( "*************Payload: NONE *****************");
     const {fromDate,toDate,userId}=req.query;
-    // SELECT t.*,c.txn_name FROM transaction_history t 
-    // LEFT JOIN transaction c ON c.txn_id=t.txn_id
-    // WHERE t.txn_his_date BETWEEN '2022-01-01 00:00:00' AND '2022-03-01 00:00:00' AND t.txn_his_inputter='1002'
+
     let extraCondition;
     if (userId.includes(null)||userId=='') {
         extraCondition=''
