bf4a311fc1c77b56e59cc9af45c1bcb9ddba2165
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-11 22:55:07 +0700

    new report transaction d

--- src/controllers/admin/report/index.js
+++ src/controllers/admin/report/index.js
@@ -3,8 +3,12 @@ const Db=require("../../../config/dbcon");
 const txnReport=async(req,res)=>{
 
     const {fromDate,toDate,userId}=req.body;
+    // SELECT t.*,c.txn_name FROM transaction_history t 
+    // LEFT JOIN transaction c ON c.txn_id=t.txn_id
+    // WHERE t.txn_his_date BETWEEN '2022-01-01 00:00:00' AND '2022-03-01 00:00:00' AND t.txn_his_inputter='1002'
+
     console.log("fdate: "+fromDate+" todate: "+toDate+" userid: "+userId);
-    const sqlCom=`SELECT * FROM transaction_history WHERE txn_his_date BETWEEN '${fromDate}' AND '${toDate}' AND txn_his_inputter='${userId}'`
+    const sqlCom=`SELECT t.*,c.txn_name FROM transaction_history t LEFT JOIN transaction c ON c.txn_id=t.txn_id WHERE t.txn_his_date BETWEEN '${fromDate}' AND '${toDate}' AND t.txn_his_inputter='${userId}'`
     console.log("SQL: "+sqlCom);
     Db.query(sqlCom,(er,re)=>{
         if(er) return res.send("Error: "+er);
