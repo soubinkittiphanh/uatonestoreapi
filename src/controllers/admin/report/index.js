b4e0b221f99d538718c2836dcc14e9e1305b7275
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-11 23:37:33 +0700

    new report transaction dssSss

--- src/controllers/admin/report/index.js
+++ src/controllers/admin/report/index.js
@@ -2,7 +2,7 @@ const Db=require("../../../config/dbcon");
 
 const txnReport=async(req,res)=>{
     console.log("**************** REPORT **************");
-    console.log( "*************Payload: NONE *****************d");
+    console.log( "*************Payload: NONE *****************");
     const {fromDate,toDate}=req.query;
     const userId = req.query.userId
     let extraCondition;
@@ -12,7 +12,7 @@ const txnReport=async(req,res)=>{
         extraCondition = ` AND t.txn_his_inputter=${userId}`
     }
     console.log("fdate: "+fromDate+" todate: "+toDate+" userid: "+userId);
-    const sqlCom=`SELECT t.*,c.txn_name FROM transaction_history t LEFT JOIN transaction c ON c.txn_id=t.txn_id WHERE t.txn_his_date BETWEEN '${fromDate}' AND '${toDate}' ${extraCondition} ORDER BY t.txn.his.date DESC`
+    const sqlCom=`SELECT t.*,c.txn_name FROM transaction_history t LEFT JOIN transaction c ON c.txn_id=t.txn_id WHERE t.txn_his_date BETWEEN '${fromDate}' AND '${toDate}' ${extraCondition} ORDER BY t.txn_his_date DESC`
     console.log("SQL: "+sqlCom);
     Db.query(sqlCom,(er,re)=>{
         if(er) return res.send("Error: "+er);
