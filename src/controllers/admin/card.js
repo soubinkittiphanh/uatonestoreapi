b0e80334362024a8282a30f92d44572605ef7dca
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-19 09:33:45 +0700

    new card activity process

--- src/controllers/admin/card.js
+++ src/controllers/admin/card.js
@@ -2,12 +2,14 @@ const Db = require('../../config/dbcon')
 const deleteCard = async (req, res) => {
     const card_id = req.body.card_id;
     const user_id = req.body.user_id;
+
     var value_date = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000).toJSON().slice(0, 19).replace('T', ' ');
 
     console.log("************* CARD DELETE *****************");
     console.log("************* DATE TIME "+value_date+ "*****************");
     console.log(`*************Payload: ${card_id} *****************`);
-    const sqlCom=`UPDATE card SET card_isused =2,update_user='${user_id}',update_time='${value_date}' WHERE id='${card_id}'`
+ 
+    const sqlCom=`UPDATE card SET card_isused =2,update_user='${user_id}',update_time='${value_date}' WHERE id='${card_id}'`;
     // const sqlCom = `INSERT INTO card_his( card_type_code, product_id, card_number, card_isused, inputter) SELECT card_type_code, product_id, card_number, card_isused, '${user_id}' FROM card WHERE card.id = '${card_id}'`
     await Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
@@ -22,9 +24,17 @@ const deleteCard = async (req, res) => {
 
 const fetchCard = async (req, res) => {
     const proId = req.query.pro_id
+    const { fDate,tDate }=req.body;
     console.log("************* LOAD CARD *****************");
     console.log(`*************Payload: ${proId} *****************`);
-    await Db.query(`SELECT c.*,u.user_name FROM card c LEFT JOIN user_account u ON u.user_id = c.inputter WHERE c.product_id='${proId}'  ORDER BY c.card_input_date DESC`, (er, re) => {
+    let sqlComCon=''
+    if(fdate&&tdate){
+        sqlComCon=`AND card_input_date BETWEEN '${fDate} 00:00:00' AND '${tDate} 23:59:59' `
+    }
+    console.log("SQL Con: "+sqlComCon);
+    const sqlCom=`SELECT c.*,u.user_name FROM card c LEFT JOIN user_account u ON u.user_id = c.inputter WHERE c.product_id='${proId}' ${sqlComCon} ORDER BY c.card_input_date DESC`
+    
+    await Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
         res.send(re);
     })
