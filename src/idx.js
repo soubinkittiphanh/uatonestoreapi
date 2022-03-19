02ac262083cde86819db4ef3835d8ffdfc5bbc30
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-19 21:45:56 +0700

    update report cardhistory viewdd

--- src/idx.js
+++ src/idx.js
@@ -2,6 +2,8 @@
 // console.log("DATE"+datea);
 // var d = new Date;
 
+
+
 // var date = [
 //     d.getFullYear(),
 //     ('00' + d.getMonth() + 1).slice(-2),
@@ -19,3 +21,12 @@
 
 var sqlDatetime = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000).toJSON().slice(0, 19).replace('T', ' ');
 console.log(sqlDatetime);
+
+const Db=require("./config/dbcon")
+Db.query("INSERT INTO card(card_type_code,product_id,card_number,card_isused,update_user,update_time,inputter) SELECT card_type_code,product_id,card_number,'2',inputter,card_input_date,inputter FROM card_his",(er,re)=>{
+    if(er){
+        console.log("ERROR: "+er);
+    }else{
+        console.log(("Transaction inserted"));
+    }
+})
