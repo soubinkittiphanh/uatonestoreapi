e6b784f73b37bda33579f838224d22c1778b7312
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-03-19 08:22:52 +0700

    new card activity process

--- src/controllers/admin/card.js
+++ src/controllers/admin/card.js
@@ -2,15 +2,18 @@ const Db = require('../../config/dbcon')
 const deleteCard = async (req, res) => {
     const card_id = req.body.card_id;
     const user_id = req.body.user_id;
+    var value_date = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000).toJSON().slice(0, 19).replace('T', ' ');
+
     console.log("************* CARD DELETE *****************");
     console.log(`*************Payload: ${card_id} *****************`);
-    const sqlCom = `INSERT INTO card_his( card_type_code, product_id, card_number, card_isused, inputter) SELECT card_type_code, product_id, card_number, card_isused, '${user_id}' FROM card WHERE card.id = '${card_id}'`
+    const sqlCom=`UPDATE card SET card_isused =2,update_user='${user_id}',update_time='${value_date}' WHERE id='${card_id}'`
+    // const sqlCom = `INSERT INTO card_his( card_type_code, product_id, card_number, card_isused, inputter) SELECT card_type_code, product_id, card_number, card_isused, '${user_id}' FROM card WHERE card.id = '${card_id}'`
     await Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
-        Db.query(`DELETE FROM card WHERE id = '${card_id}'`, (er, re) => {
-            if (er) res.send("Error: " + er);
+        // Db.query(`DELETE FROM card WHERE id = '${card_id}'`, (er, re) => {
+        //     if (er) res.send("Error: " + er);
             return res.send("Transaction completed")
-        })
+        // })
     })
 
 
