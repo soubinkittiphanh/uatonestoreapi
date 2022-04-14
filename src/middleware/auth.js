448d884206e7212ef27d1d6acb1b105d47aae778
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-04-14 14:37:09 +0700

    inspect slow down cause on placing order

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const secret=require('../config/').actksecret
+
+const validateToken=async(req,res,done)=>{
+    const {authorization} =req.headers;
+    const token =authorization&&authorization.split(" ")[1];
+    console.log("Token: "+token);
+    console.log("Secret: "+secret);
+    if (token == null) return res.json({status:"02",desc:"No token"})
+    jwt.verify(token,secret,(er,result)=>{
+        if(er) return res.json({status:"02",desc:er})
+        // res.send({"status":"00",desc:"Token is valid"})
+        done();
+
+    })
+}
+module.exports={
+    validateToken,
+}
