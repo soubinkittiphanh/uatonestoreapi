352c2aa7c3c42d07ea54289985eeab594106b759
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-04-14 15:05:33 +0700

    toke validate on order

--- src/helper/tokenHelper.js
+++ src/helper/tokenHelper.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const login = (u_name, u_id, u_phone, u_email, u_debit, u_credit,u_img) => {
     console.log('=>' + process.env.ACCESS_TOKEN_SECRET);
     const username = { user: u_name };
-    const accessToken = jwt.sign(username, Token.actksecret, { expiresIn: '2h' });
+    const accessToken = jwt.sign(username, Token.actksecret, { expiresIn: '2s' });
     return { accessToken: accessToken, userName: u_name, userId: u_id, userPhone: u_phone, userEmail: u_email, userDebit: u_debit, userCredit: u_credit,img_path:u_img}
 }
 
