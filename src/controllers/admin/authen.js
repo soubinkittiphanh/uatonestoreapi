4fbf1f5045aa3b9a9544be5a0efe2e398288f4e8
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-04-15 09:39:11 +0700

    update check version before login

--- src/controllers/admin/authen.js
+++ src/controllers/admin/authen.js
@@ -23,11 +23,8 @@ const Authcustomer = async (req, res) => {
     if(!version)return res.send('Error: ກະລຸນາອັບເດດເວີຊັ່ນໃຫມ່')
     if(version!='1.1.0')return res.send('Error: ກະລຸນາອັບເດດເວີຊັ່ນໃຫມ່')
     const body = req.body;
-    console.log(body);
     const u_id = body.cus_id;
     const u_pw = body.cus_pwd;
-    console.log("************* CUSTOMER AUTHEN }*****************");
-    console.log(`*************Payload: ${u_id} *****************`);
     // const sqlCom=`SELECT c.*,IFNULL(b.DEBIT+b.ORDER_TOTAL,0) AS debit,IFNULL(b.CREDIT,0) AS credit FROM customer c 
     // LEFT JOIN(SELECT c.cus_id,c.cus_name,h.txn_his_amount,h.user_id,h.txn_his_date,t.txn_id,t.txn_name,t.txn_code,d.txn_code_id,d.txn_code_name,d.txn_sign,SUM(IF(d.txn_sign='DR',h.txn_his_amount,0)) AS DEBIT,SUM(IF(d.txn_sign='CR',h.txn_his_amount,0))AS CREDIT,o.ORDER_TOTAL FROM customer c
     //     LEFT JOIN transaction_history h ON h.user_id=c.cus_id
@@ -47,9 +44,8 @@ const Authcustomer = async (req, res) => {
         WHERE c.login_id='${u_id}' AND c.cus_pass='${u_pw}'`;
        // const sqlCom=`SELECT * FROM customer where login_id='${u_id}' AND cus_pass='${u_pw}'`;
 
-    await Db.query(sqlCom, (er, re) => {
+    Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
-        console.log(re);
         console.log("************* AUTH SUCCEED *****************");
         re.length > 0 ? res.send(Login.login(re[0]['cus_name'],re[0]['cus_id'],re[0]['cus_tel'],re[0]['cus_email'],re[0]['debit'],re[0]['credit'],re[0]['img_path']))
             : res.send({"accessToken":"","error":"ລະຫັດຜ່ານ ຫລື ໄອດີບໍ່ຖືກຕ້ອງ"})
