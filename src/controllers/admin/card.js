ff92eae94a2571d9e83ec0b34636b250ddbbba50
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-05-16 18:16:32 +0700

    update deleted card

--- src/controllers/admin/card.js
+++ src/controllers/admin/card.js
@@ -47,6 +47,9 @@ const fetchDeletedCard = async (req, res) => {
     const { fdate, tdate ,userId} = req.query;
  var sqlDatetime = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60 * 1000).toJSON().slice(0, 19).replace('T', ' ');
 console.log(sqlDatetime);
+console.log("fdate: "+sqlDatetime.substring(0,11));
+console.log(sqlDatetime);
+
     console.log("SELECT DEL CARD: "+fdate+" tdate: "+tdate);
 
     let sqlCom = `select c.*,p.pro_name,p.pro_price,u.user_name from card c LEFT JOIN product p ON p.pro_id=c.product_id LEFT JOIN user_account u ON u.user_id=c.update_user WHERE c.card_isused=2 AND c.update_time BETWEEN '${fdate} 00:00:00' AND '${tdate} 23:59:59' `
