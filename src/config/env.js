d513861f34aae2da7f5c675228662235f06174fc
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2022-12-09 10:23:24 +0700

    domain ip

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,9 @@ const config={
     actksecret:'e3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7594b0ce6c7192907213291818a4fdc89bf605ce8',
     rfTkSecret:'e3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7',
     db:{
-        host:'mariadb-34248-0.cloudclusters.net',
+        host:'209.209.40.80',
+        // mariadb-34248-0.cloudclusters.net
+        // nodejsclusters-66380-0.cloudclusters.net
         user:'soubin',
         password:'SDAT@3480',
         // database:'online_store_pakse',
--- src/controllers/admin/authen.js
+++ src/controllers/admin/authen.js
@@ -9,7 +9,7 @@ const Authmember = async (req, res) => {
     console.log("mem_id: "+u_id);
     console.log("mem_password: "+u_pw);
     console.log("login credential: "+body);
-     Db.query(`SELECT * FROM user_account where user_id='${u_id}' AND user_pass='${u_pw}'`, (er, re) => {
+     Db.query(`SELECT * FROM user_account WHERE user_id='${u_id}' AND user_pass='${u_pw}'`, (er, re) => {
         if (er) return res.send("Error: " + er)
         re.length > 0 ? res.send(Login.login(re[0]['user_name'],re[0]['user_id'],re[0]['user_tel'],"_",0,0))
         : res.send({"accessToken":"","error":"ລະຫັດຜ່ານ ຫລື ໄອດີບໍ່ຖືກຕ້ອງ"})
@@ -25,6 +25,8 @@ const Authcustomer = async (req, res) => {
     const body = req.body;
     const u_id = body.cus_id;
     const u_pw = body.cus_pwd;
+    console.log("cus_id: "+u_id);
+    console.log("cus_pwd: "+u_pw);
     // const sqlCom=`SELECT c.*,IFNULL(b.DEBIT+b.ORDER_TOTAL,0) AS debit,IFNULL(b.CREDIT,0) AS credit FROM customer c 
     // LEFT JOIN(SELECT c.cus_id,c.cus_name,h.txn_his_amount,h.user_id,h.txn_his_date,t.txn_id,t.txn_name,t.txn_code,d.txn_code_id,d.txn_code_name,d.txn_sign,SUM(IF(d.txn_sign='DR',h.txn_his_amount,0)) AS DEBIT,SUM(IF(d.txn_sign='CR',h.txn_his_amount,0))AS CREDIT,o.ORDER_TOTAL FROM customer c
     //     LEFT JOIN transaction_history h ON h.user_id=c.cus_id
@@ -34,7 +36,8 @@ const Authcustomer = async (req, res) => {
     //     WHERE c.cus_id=(SELECT cus_id FROM customer WHERE login_id='${u_id}')) b ON b.cus_id =c.cus_id 
     //     WHERE c.login_id='${u_id}' AND c.cus_pass='${u_pw}'`;
     const sqlCom=`SELECT c.*,IFNULL(b.DEBIT+b.ORDER_TOTAL,0) AS debit,IFNULL(b.CREDIT,0) AS credit,img_path FROM customer c 
-    LEFT JOIN(SELECT c.cus_id,c.cus_name,h.txn_his_amount,h.user_id,h.txn_his_date,t.txn_id,t.txn_name,t.txn_code,d.txn_code_id,d.txn_code_name,d.txn_sign,SUM(IF(d.txn_sign='DR',h.txn_his_amount,0)) AS DEBIT,SUM(IF(d.txn_sign='CR',h.txn_his_amount,0))AS CREDIT,o.ORDER_TOTAL,i.img_path FROM customer c
+    LEFT JOIN(
+        SELECT c.cus_id,c.cus_name,h.txn_his_amount,h.user_id,h.txn_his_date,t.txn_id,t.txn_name,t.txn_code,d.txn_code_id,d.txn_code_name,d.txn_sign,SUM(IF(d.txn_sign='DR',h.txn_his_amount,0)) AS DEBIT,SUM(IF(d.txn_sign='CR',h.txn_his_amount,0))AS CREDIT,o.ORDER_TOTAL,i.img_path FROM customer c
         LEFT JOIN transaction_history h ON h.user_id=c.cus_id
         LEFT JOIN transaction t ON t.txn_id=h.txn_id
         LEFT JOIN image_path_master i ON i.app_txn_id =c.login_id
@@ -47,7 +50,9 @@ const Authcustomer = async (req, res) => {
     Db.query(sqlCom, (er, re) => {
         if (er) return res.send("Error: " + er)
         console.log("************* AUTH SUCCEED *****************");
-        re.length > 0 ? res.send(Login.login(re[0]['cus_name'],re[0]['cus_id'],re[0]['cus_tel'],re[0]['cus_email'],re[0]['debit'],re[0]['credit'],re[0]['img_path']))
+        console.log("LEN: "+re.length);
+        re.length > 0 ? res.send(Login.login(re[0]['cus_name'],re[0]['cus_id'],re[0]['cus_tel'],
+        re[0]['cus_email'],re[0]['debit'],re[0]['credit'],re[0]['img_path']))
             : res.send({"accessToken":"","error":"ລະຫັດຜ່ານ ຫລື ໄອດີບໍ່ຖືກຕ້ອງ"})
 
     })
